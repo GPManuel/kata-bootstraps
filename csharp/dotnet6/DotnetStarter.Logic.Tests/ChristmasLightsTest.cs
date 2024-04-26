e5ab0b7c73ff41643e2df068868cd5d2fa06df79
Manuel Gallud Peñalver <mgallud@aidacanarias.com>
2024-04-26 20:30:23 +0200

    ✅ VALIDATION TESTS

--- csharp/dotnet6/DotnetStarter.Logic.Tests/ChristmasLightsTest.cs
+++ csharp/dotnet6/DotnetStarter.Logic.Tests/ChristmasLightsTest.cs
@@ -64,5 +64,31 @@ namespace DotnetStarter.Logic.Tests
 
             Assert.That(_christmasLights.CountLitLights(), Is.EqualTo(1));
         }
+
+        [Test]
+        public void final_test()
+        {
+            _christmasLights.TurnOn(new Coordinates(887, 9), new Coordinates(959, 629));
+            _christmasLights.TurnOn(new Coordinates(454, 398), new Coordinates(844, 448));
+            _christmasLights.TurnOff(new Coordinates(539, 243), new Coordinates(559, 965));
+            _christmasLights.TurnOff(new Coordinates(370, 819), new Coordinates(676, 868));
+            _christmasLights.TurnOff(new Coordinates(145, 40), new Coordinates(370, 997));
+            _christmasLights.TurnOff(new Coordinates(301, 3), new Coordinates(808, 453));
+            _christmasLights.TurnOn(new Coordinates(351, 678), new Coordinates(951, 908));
+            _christmasLights.Toggle(new Coordinates(720, 196), new Coordinates(897, 994));
+            _christmasLights.Toggle(new Coordinates(831, 394), new Coordinates(904, 860));
+            
+            Assert.That(_christmasLights.CountLitLights(), Is.EqualTo(230022));
+        }
+
+        [Test]
+        public void medium_test()
+        {
+            _christmasLights.TurnOn(new Coordinates(0,0), new Coordinates(999,999));
+            _christmasLights.Toggle(new Coordinates(0,0), new Coordinates(999,0));
+            _christmasLights.TurnOff(new Coordinates(499,499), new Coordinates(500,500));
+
+            Assert.That(_christmasLights.CountLitLights(), Is.EqualTo(998996));
+        }
     }
 }
