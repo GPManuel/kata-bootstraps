9bb2e1270752d2b946092027bc85392ea594dbbd
Manuel Gallud Peñalver <mgallud@aidacanarias.com>
2024-04-27 10:56:39 +0200

    🎨 ChristmasLights

--- csharp/dotnet6/DotnetStarter.Logic/ChristmasLights.cs
+++ csharp/dotnet6/DotnetStarter.Logic/ChristmasLights.cs
@@ -6,6 +6,9 @@ namespace DotnetStarter.Logic
     {
         private const int CoordinateXMaxValue = 1000;
         private const int CoordinateYMaxValue = 1000;
+        private const int TurnOnValue = 1;
+        private const int TurnOffValue = -1;
+        private const int ToggleBrightnessValue = 2;
         private readonly Light[,] _lightsWithBrightness = new Light[CoordinateXMaxValue, CoordinateYMaxValue];
 
         public ChristmasLights()
@@ -24,45 +27,45 @@ namespace DotnetStarter.Logic
             {
                 for (var initialPositionY = startCoordinates.Y; initialPositionY <= endCoordinates.Y; initialPositionY++)
                 {
-                    ChangeLightBrightness(initialPostionX, initialPositionY, 1);
+                    ChangeLightBrightness(new Coordinates(initialPostionX, initialPositionY), TurnOnValue);
                 }
             }
         }
 
         public void TurnOff(Coordinates startCoordinates, Coordinates endCoordinates)
         {
-            for (var initialPostionX = startCoordinates.X; initialPostionX <= endCoordinates.X; initialPostionX++)
+            for (var initialPositionX = startCoordinates.X; initialPositionX <= endCoordinates.X; initialPositionX++)
             {
                 for (var initialPositionY = startCoordinates.Y; initialPositionY <= endCoordinates.Y; initialPositionY++)
                 {
-                    ChangeLightBrightness(initialPostionX, initialPositionY, -1);
+                    ChangeLightBrightness(new Coordinates(initialPositionX, initialPositionY), TurnOffValue);
                 }
             }
         }
 
         public void Toggle(Coordinates startCoordinates, Coordinates endCoordinates)
         {
-            for (var initialPostionX = startCoordinates.X; initialPostionX <= endCoordinates.X; initialPostionX++)
+            for (var initialPositionX = startCoordinates.X; initialPositionX <= endCoordinates.X; initialPositionX++)
             {
                 for (var initialPositionY = startCoordinates.Y; initialPositionY <= endCoordinates.Y; initialPositionY++)
                 {
-                    ChangeLightBrightness(initialPostionX, initialPositionY, 2);
+                    ChangeLightBrightness(new Coordinates(initialPositionX, initialPositionY), ToggleBrightnessValue);
                 }
             }
         }
 
-        private void ChangeLightBrightness(int initialPostionX, int initialPositionY, int increasedBrightness)
+        private void ChangeLightBrightness(Coordinates coordinates, int increasedBrightness)
         {
-            _lightsWithBrightness[initialPostionX, initialPositionY].ChangeBrightness(increasedBrightness);
+            _lightsWithBrightness[coordinates.X, coordinates.Y].ChangeBrightness(increasedBrightness);
         }
 
         private void InitializeChristmasLights()
         {
-            for (var initialPostionX = 0; initialPostionX < CoordinateXMaxValue; initialPostionX++)
+            for (var initialPositionX = 0; initialPositionX < CoordinateXMaxValue; initialPositionX++)
             {
                 for (var initialPositionY = 0; initialPositionY < CoordinateYMaxValue; initialPositionY++)
                 {
-                    _lightsWithBrightness[initialPostionX, initialPositionY] = new Light();
+                    _lightsWithBrightness[initialPositionX, initialPositionY] = new Light();
                 }
             }
         }
